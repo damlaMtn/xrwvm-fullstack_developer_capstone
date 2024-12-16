0031310a63dc4af12e7050c2b926af45bc498dee
damlaMtn <damla91@gmail.com>
2024-12-16 17:09:18 -0500

    build carmodel and carmake django models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,44 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Car Model model
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    CONVERTIBLE = 'Convertible'
+    HATCHBACK = 'Hatchback'
 
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (CONVERTIBLE, 'Convertible'),
+        (HATCHBACK, 'Hatchback'),
+    ]
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
+    name = models.CharField(max_length=100)
+    car_type = models.CharField(max_length=50, choices=CAR_TYPE_CHOICES)
+    year = models.IntegerField()
+
+    def clean(self):
+        super().clean()
+        if self.year < 2015 or self.year > 2023:
+            raise ValueError("Year must be between 2015 and 2023.")
+
+    def __str__(self):
+        return f"{self.name} ({self.car_make.name})"
+
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,12 +15,25 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 
+from .models import CarMake, CarModel
+
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print("count: ", count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -75,6 +88,7 @@ def registration(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
